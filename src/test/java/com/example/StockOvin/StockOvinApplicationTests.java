73ef2c756ff52bc24942423d3301d1f3c69a53df
Toray <damien.pasquer3636@gmail.com>
2024-05-13 11:35:44 +0200

    base

---
+++ src/test/java/com/example/StockOvin/StockOvinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StockOvin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockOvinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
