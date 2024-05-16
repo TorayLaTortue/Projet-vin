3f68d047eb0243c8a68e9005c05e419744967f65
Clém <clemauger01@orange.fr>
2024-05-16 15:02:02 +0200

    Merge remote-tracking branch 'origin/Develop' into Develop

--- src/main/java/com/example/StockOvin/Controllers/VinController.java
+++ src/main/java/com/example/StockOvin/Controllers/VinController.java
@@ -3,12 +3,7 @@ package com.example.StockOvin.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
 import java.util.List;
@@ -17,7 +12,7 @@ import com.example.StockOvin.Entities.VinEntity;
 import com.example.StockOvin.Service.VinService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @RequestMapping("/Vin")
@@ -35,17 +30,20 @@ public class VinController {
 
     @Operation(summary = "Ajout d'un vin (Nom, Image, Region, Annee, Quantite, Format, Prix)")
     @PostMapping("/Post")
-    public VinEntity AddVin(@RequestBody VinEntity newVin) {
+    public VinEntity newVin(@RequestBody VinEntity newVin) {
         VinEntity Vin = new VinEntity();
         Vin.setNom(newVin.getNom());
         Vin.setImage(newVin.getImage());
         Vin.setRegion(newVin.getRegion());
         Vin.setAnnee(newVin.getAnnee());
         Vin.setQuantite(newVin.getQuantite());
+        Vin.setFamille(newVin.getFamille());
         Vin.setFormat(newVin.getFormat());
         Vin.setPrix(newVin.getPrix());
         return VinService.AddVin(Vin); 
     }
+
+
 
     @Operation(summary = "Edit d'un vin (Nom, Image, Region, Annee, Quantite, Format, Prix)")
     @PutMapping("/Update/{id}")
