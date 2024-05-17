c48ca4ed223bf9ef91d32351157ce72640c7f6cc
AFROUNE MOHAMED ADAM <148912667+adam1508042001@users.noreply.github.com>
2024-05-17 13:22:31 +0200

    routes dans le meme fichier, ajout de statut et date de creation de commande.

--- src/main/java/com/example/StockOvin/Repository/PostCommandeRepository.java
+++ src/main/java/com/example/StockOvin/Repository/PostCommandeRepository.java
@@ -1,10 +1,8 @@
 package com.example.StockOvin.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.example.StockOvin.Entities.PostCommandeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
 public interface PostCommandeRepository extends JpaRepository<PostCommandeEntity, Long> {
 
 }
--- src/main/java/com/example/StockOvin/Repository/UpdateCommandeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.StockOvin.Repository;
-
-import com.example.StockOvin.Entities.UpdateCommandeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UpdateCommandeRepository extends JpaRepository<UpdateCommandeEntity, Integer> {
-}
