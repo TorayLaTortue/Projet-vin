c210908dbd2a8b2946036dfa67ff28deff06d933
clement14A <148197331+clement14A@users.noreply.github.com>
2024-06-05 11:43:15 +0200

    Update Table.sql

--- Sql/Table.sql
+++ Sql/Table.sql
@@ -1,34 +1,34 @@
 CREATE TABLE IF NOT EXISTS public.client
 (
     "client_reference" SERIAL PRIMARY KEY,
-    "name" character(100) COLLATE pg_catalog."default",
-    "first_name" character(100) COLLATE pg_catalog."default",
-    "email" character(100) COLLATE pg_catalog."default",
-    "phone" character(20) COLLATE pg_catalog."default",
-    "password" character(20) COLLATE pg_catalog."default",
-    "role" character(100) COLLATE pg_catalog."default",
+    "name" varchar(100) COLLATE pg_catalog."default",
+    "first_name" varchar(100) COLLATE pg_catalog."default",
+    "email" varchar(100) COLLATE pg_catalog."default",
+    "phone" varchar(20) COLLATE pg_catalog."default",
+    "password" varchar(20) COLLATE pg_catalog."default",
+    "role" varchar(100) COLLATE pg_catalog."default",
     "deletion_date" date
 );
 
 CREATE TABLE IF NOT EXISTS public.address
 (
     "address_reference" SERIAL PRIMARY KEY,
-    "city" character(100) COLLATE pg_catalog."default",
-    "street" character(100) COLLATE pg_catalog."default",
-    "postal_code" character(20) COLLATE pg_catalog."default",
+    "city" varchar(100) COLLATE pg_catalog."default",
+    "street" varchar(100) COLLATE pg_catalog."default",
+    "postal_code" varchar(20) COLLATE pg_catalog."default",
     "deletion_date" date
 );
 
 CREATE TABLE IF NOT EXISTS public.wine
 (
     "wine_reference" SERIAL PRIMARY KEY,
-    "name" character(100) COLLATE pg_catalog."default",
-    "wine_quality" character(100) COLLATE pg_catalog."default",
-    "picture" character(1000) COLLATE pg_catalog."default",
-    "region" character(100) COLLATE pg_catalog."default",
-    "family" character(100) COLLATE pg_catalog."default",
+    "name" varchar(100) COLLATE pg_catalog."default",
+    "wine_quality" varchar(100) COLLATE pg_catalog."default",
+    "picture" varchar(1000) COLLATE pg_catalog."default",
+    "region" varchar(100) COLLATE pg_catalog."default",
+    "family" varchar(100) COLLATE pg_catalog."default",
     "year" date,
-    "format" character(100) COLLATE pg_catalog."default",
+    "format" varchar(100) COLLATE pg_catalog."default",
     "discount" float,
     "price" integer,
     "deletion_date" date
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS public.orders
     "client_reference" integer REFERENCES public.client ("client_reference"),
     "billing_address" integer REFERENCES public.address ("address_reference"),
     "delivery_address" integer REFERENCES public.address ("address_reference"),
-    "status" character(100) COLLATE pg_catalog."default",
+    "status" varchar(100) COLLATE pg_catalog."default",
     "order_creation_date" date,
     "wine_reference" integer REFERENCES public.wine ("wine_reference"),
     "quantity_product" integer,
@@ -52,7 +52,7 @@ CREATE TABLE IF NOT EXISTS public.supplier
     "supplier_reference" SERIAL PRIMARY KEY,
     "wine_reference" integer REFERENCES public.wine ("wine_reference"),
     "client_reference" integer REFERENCES public.client ("client_reference"),
-    "name" character(100) COLLATE pg_catalog."default",
+    "name" varchar(100) COLLATE pg_catalog."default",
     "address" integer REFERENCES public.address ("address_reference"),
     "deletion_date" date
 );
@@ -60,7 +60,7 @@ CREATE TABLE IF NOT EXISTS public.supplier
 CREATE TABLE IF NOT EXISTS public.deposit
 (
     "deposit_reference" SERIAL PRIMARY KEY,
-    "deposit_name" character(100) COLLATE pg_catalog."default",
+    "deposit_name" varchar(100) COLLATE pg_catalog."default",
     "address" integer REFERENCES public.address ("address_reference"),
     "wine_reference" integer REFERENCES public.wine ("wine_reference"),
     "quantity" integer,
